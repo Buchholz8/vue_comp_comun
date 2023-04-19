3b5002832cd5dedb35ff8c0a3c4cdaac70ae3cfa
markb <markbuchholz8@gmail.com>
2023-04-18 19:31:41 -0600

    new save

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    <h1> {{username}} </h1>
+    <small-user-profile v-for="(user , i) in users" :key="i" :user="users[i]">
+
+    </small-user-profile>
+  </div>
+</template>
+
+<script>
+import SmallUserProfile from '@/components/SmallUserProfile.vue'
+
+export default {
+  methods: {
+    handle_username: function(name){
+this.username = name
+    },
+  },
+  data(){
+    return {
+      username: undefined,
+      users: [
+        {
+        name: `Frank`,
+        age: 21,
+        is_premium: false,
+        id:1
+      },
+      {
+        name: `jeff`,
+        age: 28,
+        is_premium: true,
+        id:2
+      }
+      ]
+    }
+  },
+  name: 'App',
+  components: {
+    SmallUserProfile,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/SmallUserProfile.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+       <h3> {{user[`name`]}} </h3>
+       <p> {{user[`age`]}} </p>
+       <h5 v-if="user[`is_premium`] === true"> Premium</h5>
+       <p v-else> basic</p>
+       <button @click="user_clicked"> Click Me</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        methods: {
+            user_clicked( ) {
+                this.$emit(`user_clciked` , this.user[`name`])
+            },
+        },
+        props: {
+            user: Object,
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
